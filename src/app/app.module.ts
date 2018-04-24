aaae3defb5a0a8e93c65beef20aa40f7b7e739f5
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2018-04-24 15:23:38 +0900

    Add frontpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import {RouterModule, Routes} from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { SourceSelectComponent } from './frontpage/source-select/source-select.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 
 const appRoutes: Routes = [
   { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: '/about', pathMatch: 'full'}
+  { path: '', pathMatch: 'full', component: FrontpageComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    SourceSelectComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
