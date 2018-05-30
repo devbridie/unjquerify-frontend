b1607e7810c615990e2de2c1eb449510be6bda38
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2018-05-30 13:35:18 +0900

    Fix messy reactivity in SourceSelectComponent

--- src/app/frontpage/source-select/source-select.component.ts
+++ src/app/frontpage/source-select/source-select.component.ts
@@ -1,29 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {BehaviorSubject} from 'rxjs/index';
+import {ExampleRetrieverService} from '../../example-retriever.service';
+import {switchMap} from 'rxjs/operators';
 
-type UploadType = 'Example'| 'URL'| 'Input'| 'Upload';
+type UploadType = 'Example' | 'URL' | 'Input' | 'Upload';
 
 @Component({
   selector: 'app-source-select',
   templateUrl: './source-select.component.html',
   styleUrls: ['./source-select.component.scss']
 })
-export class SourceSelectComponent implements OnInit {
-  uploadTypes = ['Example', 'Input', /* 'URL', 'Upload'*/];
+export class SourceSelectComponent {
+  uploadTypes: UploadType[] = ['Example', 'Input', /* 'URL', 'Upload'*/];
+  selectedUploadType$ = new BehaviorSubject(this.uploadTypes[0]);
+
   examples = ['simple', 'characterCounter'];
-  selected = this.uploadTypes[0];
+  selectedExample$ = new BehaviorSubject(this.examples[0]);
+  exampleCode$ = this.selectedExample$.pipe(switchMap(name => this.exampleRetriever.getExample(name)));
 
   inputPlaceholder = localStorage.getItem('input') || '';
 
-  constructor(private router: Router) {
-  }
-
-  ngOnInit() {
-  }
-
-  setSelected(selected: UploadType) {
-    this.selected = selected;
-    return false;
+  constructor(private router: Router, private exampleRetriever: ExampleRetrieverService) {
   }
 
   submitText(text: string) {
