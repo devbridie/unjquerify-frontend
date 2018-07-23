85df36d582cbd2fe6b5ff5469d9a27e63494c6eb
Dereck Bridie <d.j.bridie@student.tudelft.nl>
2018-07-23 14:39:07 +0900

    Add branding

--- src/app/documentation/plugins/reference.ts
+++ src/app/documentation/plugins/reference.ts
@@ -1,18 +1,31 @@
 export interface Reference {
   type: ReferenceType;
   url: string;
+  display: string;
 }
 
 export type ReferenceType = 'jquery' | 'mdn' | 'caniuse' | 'youdon\'tneedjquery';
 
 export function youDontNeedJquery(section: string): Reference {
-  return { type: 'youdon\'tneedjquery', url: 'https://github.com/nefe/You-Dont-Need-jQuery#' + section };
+  return {
+    type: 'youdon\'tneedjquery',
+    url: 'https://github.com/nefe/You-Dont-Need-jQuery#' + section,
+    display: `You Don't Need jQuery: §${section}`,
+  };
 }
 
 export function mdnReference(subPath: string): Reference {
-  return { type: 'mdn', url: 'https://developer.mozilla.org/en-US/docs/Web/API/' + subPath + '/' };
+  return {
+    type: 'mdn',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/API/' + subPath + '/',
+    display: `MDN Web Docs: <code>${subPath.replace('/', '.')}</code>`
+  };
 }
 
 export function jqueryApiReference(subPath: string): Reference {
-  return { type: 'jquery', url: 'https://api.jquery.com/' + subPath + '/' };
+  return {
+    type: 'jquery',
+    url: 'https://api.jquery.com/' + subPath + '/',
+    display: `jQuery Documentation: <code>${subPath}</code>`,
+  };
 }
